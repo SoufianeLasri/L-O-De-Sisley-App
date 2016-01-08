3fafc025d1f38276f404bebe1b8ea2a8c16e22fb
SoufianeLasri <soufiane.lasri@gmail.com>
2016-01-08 04:44:01 +0100

    Fix splashscreen and add intro animations

--- Sisley/ConnexionView.swift
+++ Sisley/ConnexionView.swift
@@ -9,106 +9,163 @@
 import UIKit
 
 class ConnexionController: UIViewController {
+    
+    var customTitle: CustomTitle!
+    var imageView: UIImageView!
+    var appName: UILabel!
+    var mailLabel: UILabel!
+    var firstLine: CAShapeLayer!
+    var passwordLabel: UILabel!
+    var secondLine: CAShapeLayer!
+    var facebookConnectButton: SharingButton!
+    var validateButton: ValidateButton!
+    var passwordForgottenLabel: UILabel!
+    var createAccountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor( red: 1.0, green: 0.98, blue: 0.96, alpha: 1.0 )
         
-        let title = CustomTitle( frame: CGRect( x: 0, y: 30, width: self.view.frame.width, height: 30 ), color: UIColor( red: 0.36, green: 0.37, blue: 0.53, alpha: 1.0 ) )
-        title.text = "Connexion"
-        title.font = UIFont( name: "Santana", size: 20.0 )
-        title.textAlignment = .Center
-        title.textColor = UIColor( red: 0.46, green: 0.51, blue: 0.66, alpha: 1.0 )
-        self.view.addSubview( title )
+        self.customTitle = CustomTitle( frame: CGRect( x: 0, y: 30, width: self.view.frame.width, height: 30 ), color: UIColor( red: 0.36, green: 0.37, blue: 0.53, alpha: 1.0 ) )
+        self.customTitle.text = "Connexion"
+        self.customTitle.font = UIFont( name: "Santana", size: 20.0 )
+        self.customTitle.textAlignment = .Center
+        self.customTitle.textColor = UIColor( red: 0.46, green: 0.51, blue: 0.66, alpha: 1.0 )
+        self.view.addSubview( self.customTitle )
         
-        let imageView = UIImageView( frame: CGRect( x: 0, y: 155, width: 100, height: 100 ) )
-        imageView.image = UIImage( named: "sisleyLogo.png" )
-        imageView.center.x = self.view.center.x
-        self.view.addSubview( imageView )
+        self.imageView = UIImageView( frame: CGRect( x: 0, y: 155, width: 97, height: 97 ) )
+        self.imageView.image = UIImage( named: "sisleyLogo.png" )
+        self.imageView.center.x = self.view.center.x
+        self.view.addSubview( self.imageView )
         
-        let appName = UILabel( frame: CGRect( x: 0, y: 260, width: self.view.frame.width, height: 40 ) )
-        appName.text = "L'O de Sisley"
-        appName.font = UIFont( name: "Bellota", size: 25.0 )
-        appName.textAlignment = .Center
-        appName.textColor = UIColor( red: 0.89, green: 0.81, blue: 0.47, alpha: 1.0 )
-        self.view.addSubview( appName )
+        self.appName = UILabel( frame: CGRect( x: 0, y: 260, width: self.view.frame.width, height: 40 ) )
+        self.appName.text = "L'O de Sisley"
+        self.appName.font = UIFont( name: "Bellota", size: 27.0 )
+        self.appName.textAlignment = .Center
+        self.appName.textColor = UIColor( red: 0.89, green: 0.81, blue: 0.47, alpha: 1.0 )
+        self.view.addSubview( self.appName )
 
-        let mailLabel = UILabel( frame: CGRect( x: 60, y: 330, width: self.view.frame.width, height: 30 ) )
-        mailLabel.text = "soufiane.lasri@gmail.com"
-        mailLabel.font = UIFont( name: "Bellota-Regular", size: 16.0 )
-        mailLabel.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 )
-        self.view.addSubview( mailLabel )
+        self.mailLabel = UILabel( frame: CGRect( x: 60, y: 330, width: self.view.frame.width, height: 30 ) )
+        self.mailLabel.text = "soufiane.lasri@gmail.com"
+        self.mailLabel.font = UIFont( name: "Bellota-Regular", size: 16.0 )
+        self.mailLabel.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 )
+        self.view.addSubview( self.mailLabel )
         
         let firstLinePath = UIBezierPath()
         firstLinePath.moveToPoint( CGPoint( x: 60, y: 370 ) )
         firstLinePath.addLineToPoint( CGPoint( x: self.view.frame.width - 60, y: 370 ) )
         
-        let firstLine = CAShapeLayer()
-        firstLine.path = firstLinePath.CGPath
-        firstLine.lineWidth = 1.0
-        firstLine.lineCap = kCALineJoinRound
-        firstLine.strokeColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 ).CGColor
-        self.view.layer.addSublayer( firstLine )
+        self.firstLine = CAShapeLayer()
+        self.firstLine.path = firstLinePath.CGPath
+        self.firstLine.lineWidth = 1.0
+        self.firstLine.lineCap = kCALineJoinRound
+        self.firstLine.strokeColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 ).CGColor
+        self.view.layer.addSublayer( self.firstLine )
         
-        let passwordLabel = UILabel( frame: CGRect( x: 60, y: 385, width: self.view.frame.width, height: 30 ) )
-        passwordLabel.text = "·········"
-        passwordLabel.font = UIFont( name: "Bellota-Bold", size: 27.0 )
-        passwordLabel.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 )
-        self.view.addSubview( passwordLabel )
+        self.passwordLabel = UILabel( frame: CGRect( x: 60, y: 385, width: self.view.frame.width, height: 30 ) )
+        self.passwordLabel.text = "·········"
+        self.passwordLabel.font = UIFont( name: "Bellota-Bold", size: 27.0 )
+        self.passwordLabel.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 )
+        self.view.addSubview( self.passwordLabel )
         
         let secondLinePath = UIBezierPath()
         secondLinePath.moveToPoint( CGPoint( x: 60, y: 420 ) )
         secondLinePath.addLineToPoint( CGPoint( x: self.view.frame.width - 60, y: 420 ) )
         
-        let secondLine = CAShapeLayer()
-        secondLine.path = secondLinePath.CGPath
-        secondLine.lineWidth = 1.0
-        secondLine.lineCap = kCALineJoinRound
-        secondLine.strokeColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 ).CGColor
-        self.view.layer.addSublayer( secondLine )
+        self.secondLine = CAShapeLayer()
+        self.secondLine.path = secondLinePath.CGPath
+        self.secondLine.lineWidth = 1.0
+        self.secondLine.lineCap = kCALineJoinRound
+        self.secondLine.strokeColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 ).CGColor
+        self.view.layer.addSublayer( self.secondLine )
         
-        let facebookConnectButton = SharingButton( frame: CGRect( x: 0, y: 0, width: 50, height: 50 ), imageName: "facebook.png"  )
-        facebookConnectButton.alpha = 1.0
-        facebookConnectButton.center = CGPoint( x: self.view.center.x - 40, y: 475 )
-        self.view.addSubview( facebookConnectButton )
+        self.facebookConnectButton = SharingButton( frame: CGRect( x: 0, y: 0, width: 50, height: 50 ), imageName: "facebook.png"  )
+        self.facebookConnectButton.alpha = 1.0
+        self.facebookConnectButton.center = CGPoint( x: self.view.center.x - 40, y: 480 )
+        self.view.addSubview( self.facebookConnectButton )
         
-        let validateButton = ValidateButton( frame: CGRect( x: 0, y: 0, width: 50, height: 50 ), color: UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 ) )
-        validateButton.center = CGPoint( x: self.view.center.x + 40, y: 475 )
+        self.validateButton = ValidateButton( frame: CGRect( x: 0, y: 0, width: 50, height: 50 ), color: UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 ) )
+        self.validateButton.center = CGPoint( x: self.view.center.x + 40, y: 480 )
         let validateTap = UITapGestureRecognizer( target: self, action: "goToFlower:" )
-        validateButton.addGestureRecognizer( validateTap )
-        self.view.addSubview( validateButton )
+        self.validateButton.addGestureRecognizer( validateTap )
+        self.view.addSubview( self.validateButton )
         
-        let passwordForgottenLabel = UILabel( frame: CGRect( x: 0, y: self.view.frame.height - 90, width: self.view.frame.width, height: 40 ) )
-        passwordForgottenLabel.text = "Mot de passe oublié ?"
-        passwordForgottenLabel.numberOfLines = 2
-        passwordForgottenLabel.textAlignment = .Center
-        passwordForgottenLabel.font = UIFont( name: "Bellota-Bold", size: 15.0 )
-        passwordForgottenLabel.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.53, alpha: 1.0 )
+        self.passwordForgottenLabel = UILabel( frame: CGRect( x: 0, y: self.view.frame.height - 90, width: self.view.frame.width, height: 40 ) )
+        self.passwordForgottenLabel.text = "Mot de passe oublié ?"
+        self.passwordForgottenLabel.numberOfLines = 2
+        self.passwordForgottenLabel.textAlignment = .Center
+        self.passwordForgottenLabel.font = UIFont( name: "Bellota-Bold", size: 15.0 )
+        self.passwordForgottenLabel.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.53, alpha: 1.0 )
         var underlineAttribute = [ NSUnderlineStyleAttributeName: NSUnderlineStyle.StyleSingle.rawValue ]
-        var underlineAttributedString = NSAttributedString( string: passwordForgottenLabel.text!, attributes: underlineAttribute )
-        passwordForgottenLabel.attributedText = underlineAttributedString
-        passwordForgottenLabel.alpha = 0.6
-        self.view.addSubview( passwordForgottenLabel )
+        var underlineAttributedString = NSAttributedString( string: self.passwordForgottenLabel.text!, attributes: underlineAttribute )
+        self.passwordForgottenLabel.attributedText = underlineAttributedString
+        self.passwordForgottenLabel.alpha = 0.6
+        self.view.addSubview( self.passwordForgottenLabel )
         
-        let createAccountLabel = UILabel( frame: CGRect( x: 0, y: self.view.frame.height - 60, width: self.view.frame.width, height: 40 ) )
-        createAccountLabel.text = "Se créer un compte"
-        createAccountLabel.numberOfLines = 2
-        createAccountLabel.textAlignment = .Center
-        createAccountLabel.font = UIFont( name: "Bellota-Bold", size: 15.0 )
-        createAccountLabel.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.53, alpha: 1.0 )
+        self.createAccountLabel = UILabel( frame: CGRect( x: 0, y: self.view.frame.height - 60, width: self.view.frame.width, height: 40 ) )
+        self.createAccountLabel.text = "Se créer un compte"
+        self.createAccountLabel.numberOfLines = 2
+        self.createAccountLabel.textAlignment = .Center
+        self.createAccountLabel.font = UIFont( name: "Bellota-Bold", size: 15.0 )
+        self.createAccountLabel.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.53, alpha: 1.0 )
         underlineAttribute = [ NSUnderlineStyleAttributeName: NSUnderlineStyle.StyleSingle.rawValue ]
-        underlineAttributedString = NSAttributedString( string: createAccountLabel.text!, attributes: underlineAttribute )
-        createAccountLabel.attributedText = underlineAttributedString
-        createAccountLabel.alpha = 0.6
-        self.view.addSubview( createAccountLabel )
+        underlineAttributedString = NSAttributedString( string: self.createAccountLabel.text!, attributes: underlineAttribute )
+        self.createAccountLabel.attributedText = underlineAttributedString
+        self.createAccountLabel.alpha = 0.6
+        self.view.addSubview( self.createAccountLabel )
+        
+        self.customTitle.alpha = 0.0
+//        self.imageView.alpha = 0.0
+//        self.appName.alpha = 0.0
+        self.mailLabel.alpha = 0.0
+        self.firstLine.opacity = 0.0
+        self.passwordLabel.alpha = 0.0
+        self.secondLine.opacity = 0.0
+        self.facebookConnectButton.alpha = 0.0
+        self.validateButton.alpha = 0.0
+        self.passwordForgottenLabel.alpha = 0.0
+        self.createAccountLabel.alpha = 0.0
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration( 0.5, delay: 0.5, options: .TransitionNone, animations: {
+            self.customTitle.alpha = 1.0
+            self.imageView.alpha = 1.0
+            self.appName.alpha = 1.0
+            self.mailLabel.alpha = 1.0
+            self.firstLine.opacity = 1.0
+            self.passwordLabel.alpha = 1.0
+            self.secondLine.opacity = 1.0
+            self.facebookConnectButton.alpha = 1.0
+            self.facebookConnectButton.frame.origin.y -= 5.0
+            self.validateButton.alpha = 1.0
+            self.validateButton.frame.origin.y -= 5.0
+            self.passwordForgottenLabel.alpha = 1.0
+            self.createAccountLabel.alpha = 1.0
+        }, completion: nil )
     }
     
     func goToFlower( recognizer: UITapGestureRecognizer ) {
         if recognizer.state == .Ended {
-            let storyboard: UIStoryboard = UIStoryboard( name: "Flower", bundle: nil )
-            let vc = storyboard.instantiateViewControllerWithIdentifier( "FlowerPage" ) as! FlowerController
-            self.navigationController?.pushViewController(vc, animated: false)
-//            self.presentViewController( vc, animated: false, completion: nil )
+            UIView.animateWithDuration( 0.5, delay: 0.0, options: .TransitionNone, animations: {
+                self.customTitle.alpha = 0.0
+                self.imageView.alpha = 0.0
+                self.appName.alpha = 0.0
+                self.mailLabel.alpha = 0.0
+                self.firstLine.opacity = 0.0
+                self.passwordLabel.alpha = 0.0
+                self.secondLine.opacity = 0.0
+                self.facebookConnectButton.alpha = 0.0
+                self.validateButton.alpha = 0.0
+                self.passwordForgottenLabel.alpha = 0.0
+                self.createAccountLabel.alpha = 0.0
+            }, completion: { finished in
+                let storyboard: UIStoryboard = UIStoryboard( name: "Flower", bundle: nil )
+                let vc = storyboard.instantiateViewControllerWithIdentifier( "FlowerPage" ) as! FlowerController
+                self.navigationController?.pushViewController(vc, animated: false)
+                //            self.presentViewController( vc, animated: false, completion: nil )
+            } )
         }
     }
 }
--- Sisley/IntroController.swift
+++ Sisley/IntroController.swift
@@ -9,75 +9,119 @@
 import UIKit
 
 class IntroController: UIViewController {
+    
+    var customTitle: CustomTitle!
+    var imageView: UIImageView!
+    var appName: UILabel!
+    var welcomeText: UILabel!
+    var subtitleText: UILabel!
+    var line: CAShapeLayer!
+    var tipDetails: UILabel!
+    var nextButton: NextButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor( red: 1.0, green: 0.98, blue: 0.96, alpha: 1.0 )
         
-        let title = CustomTitle( frame: CGRect( x: 0, y: 30, width: self.view.frame.width, height: 30 ), color: UIColor( red: 0.36, green: 0.37, blue: 0.53, alpha: 1.0 ) )
-        title.text = "Présentation"
-        title.font = UIFont( name: "Santana", size: 20.0 )
-        title.textAlignment = .Center
-        title.textColor = UIColor( red: 0.46, green: 0.51, blue: 0.66, alpha: 1.0 )
-        self.view.addSubview( title )
+        self.customTitle = CustomTitle( frame: CGRect( x: 0, y: 30, width: self.view.frame.width, height: 30 ), color: UIColor( red: 0.36, green: 0.37, blue: 0.53, alpha: 1.0 ) )
+        self.customTitle.text = "Présentation"
+        self.customTitle.font = UIFont( name: "Santana", size: 20.0 )
+        self.customTitle.textAlignment = .Center
+        self.customTitle.textColor = UIColor( red: 0.46, green: 0.51, blue: 0.66, alpha: 1.0 )
+        self.customTitle.alpha = 0.0
+        self.view.addSubview( self.customTitle )
         
-        let imageView = UIImageView( frame: CGRect( x: 0, y: 155, width: 100, height: 100 ) )
-        imageView.image = UIImage( named: "sisleyLogo.png" )
-        imageView.center.x = self.view.center.x
-        self.view.addSubview( imageView )
+        self.imageView = UIImageView( frame: CGRect( x: 0, y: 250, width: 97, height: 97 ) )
+        self.imageView.image = UIImage( named: "sisleyLogo.png" )
+        self.imageView.center.x = self.view.center.x
+        self.view.addSubview( self.imageView )
         
-        let appName = UILabel( frame: CGRect( x: 0, y: 260, width: self.view.frame.width, height: 40 ) )
-        appName.text = "L'O de Sisley"
-        appName.font = UIFont( name: "Bellota", size: 25.0 )
-        appName.textAlignment = .Center
-        appName.textColor = UIColor( red: 0.89, green: 0.81, blue: 0.47, alpha: 1.0 )
-        self.view.addSubview( appName )
+        self.appName = UILabel( frame: CGRect( x: 0, y: 357, width: self.view.frame.width, height: 40 ) )
+        self.appName.text = "L'O de Sisley"
+        self.appName.font = UIFont( name: "Bellota", size: 27.0 )
+        self.appName.textAlignment = .Center
+        self.appName.textColor = UIColor( red: 0.89, green: 0.81, blue: 0.47, alpha: 1.0 )
+        self.view.addSubview( self.appName )
         
-        let welcomeText = UILabel( frame: CGRect( x: 0, y: 355, width: self.view.frame.width, height: 30 ) )
-        welcomeText.text = "Bienvenue"
-        welcomeText.font = UIFont( name: "Santana-Bold", size: 26.0 )
-        welcomeText.textAlignment = .Center
-        welcomeText.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 )
-        self.view.addSubview( welcomeText )
+        self.welcomeText = UILabel( frame: CGRect( x: 0, y: 355, width: self.view.frame.width, height: 30 ) )
+        self.welcomeText.text = "Bienvenue"
+        self.welcomeText.font = UIFont( name: "Santana-Bold", size: 26.0 )
+        self.welcomeText.textAlignment = .Center
+        self.welcomeText.textColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 )
+        self.welcomeText.alpha = 0.0
+        self.view.addSubview( self.welcomeText )
         
-        let subtitleText = UILabel( frame: CGRect( x: 0, y: 380, width: self.view.frame.width, height: 30 ) )
-        subtitleText.text = "Vivez la nouvelle expérience Sisley"
-        subtitleText.font = UIFont( name: "Bellota-Italic", size: 18.0 )
-        subtitleText.textAlignment = .Center
-        subtitleText.textColor = UIColor( red: 0.89, green: 0.81, blue: 0.47, alpha: 1.0 )
-        self.view.addSubview( subtitleText )
+        self.subtitleText = UILabel( frame: CGRect( x: 0, y: 380, width: self.view.frame.width, height: 30 ) )
+        self.subtitleText.text = "Vivez la nouvelle expérience Sisley"
+        self.subtitleText.font = UIFont( name: "Bellota-Italic", size: 18.0 )
+        self.subtitleText.textAlignment = .Center
+        self.subtitleText.textColor = UIColor( red: 0.89, green: 0.81, blue: 0.47, alpha: 1.0 )
+        self.subtitleText.alpha = 0.0
+        self.view.addSubview( self.subtitleText )
         
         let linePath = UIBezierPath()
         linePath.moveToPoint( CGPoint( x: self.view.frame.width / 2 - 35, y: 430 ) )
         linePath.addLineToPoint( CGPoint( x: self.view.frame.width / 2 + 35, y: 430 ) )
         
-        let line = CAShapeLayer()
-        line.path = linePath.CGPath
-        line.lineWidth = 1.0
-        line.lineCap = kCALineJoinRound
-        line.strokeColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 ).CGColor
-        self.view.layer.addSublayer( line )
+        self.line = CAShapeLayer()
+        self.line.path = linePath.CGPath
+        self.line.lineWidth = 1.0
+        self.line.lineCap = kCALineJoinRound
+        self.line.strokeColor = UIColor( red: 0.36, green: 0.37, blue: 0.54, alpha: 1.0 ).CGColor
+        self.line.opacity = 0.0
+        self.view.layer.addSublayer( self.line )
         
-        let tipDetails = UILabel( frame: CGRect( x: 0, y: 435, width: self.view.frame.width, height: 110 ) )
-        tipDetails.text = "L’O de Sisley est une expérience inédite\nqui vous accompagnera jour après jour afin de\nvous apporter des conseils personnalisés pour\naméliorer votre bien-être."
-        tipDetails.numberOfLines = 6
-        tipDetails.font = UIFont( name: "Bellota-Regular", size: 15.0 )
-        tipDetails.textAlignment = .Center
-        tipDetails.textColor = UIColor( red: 0.46, green: 0.51, blue: 0.66, alpha: 1.0 )
-        self.view.addSubview( tipDetails )
+        self.tipDetails = UILabel( frame: CGRect( x: 0, y: 435, width: self.view.frame.width, height: 110 ) )
+        self.tipDetails.text = "L’O de Sisley est une expérience inédite\nqui vous accompagnera jour après jour afin de\nvous apporter des conseils personnalisés pour\naméliorer votre bien-être."
+        self.tipDetails.numberOfLines = 6
+        self.tipDetails.font = UIFont( name: "Bellota-Regular", size: 15.0 )
+        self.tipDetails.textAlignment = .Center
+        self.tipDetails.textColor = UIColor( red: 0.46, green: 0.51, blue: 0.66, alpha: 1.0 )
+        self.tipDetails.alpha = 0.0
+        self.view.addSubview( self.tipDetails )
         
-        let nextButton = NextButton( frame: CGRect( x: 0, y: 570, width: 50, height: 50 ) )
-        nextButton.center.x = self.view.center.x
+        self.nextButton = NextButton( frame: CGRect( x: 0, y: 575, width: 50, height: 50 ) )
+        self.nextButton.center.x = self.view.center.x
         let nextTap = UITapGestureRecognizer( target: self, action: "goToConnexion:" )
-        nextButton.addGestureRecognizer( nextTap )
-        self.view.addSubview( nextButton )
+        self.nextButton.addGestureRecognizer( nextTap )
+        self.nextButton.alpha = 0.0
+        self.view.addSubview( self.nextButton )
         
         self.navigationController?.navigationBarHidden = true
     }
     
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration( 0.5, delay: 0.5, options: .TransitionNone, animations: {
+            self.imageView.frame.origin.y = 155
+            self.appName.frame.origin.y = 260
+        }, completion: { finished in
+            UIView.animateWithDuration( 0.5, animations: {
+                self.customTitle.alpha = 1.0
+                self.welcomeText.alpha = 1.0
+                self.subtitleText.alpha = 1.0
+                self.line.opacity = 1.0
+                self.tipDetails.alpha = 1.0
+            } )
+        } )
+        
+        UIView.animateWithDuration( 0.5, delay: 1.5, options: .TransitionNone, animations: {
+            self.nextButton.alpha = 1.0
+            self.nextButton.frame.origin.y -= 5
+        }, completion: nil )
+    }
+    
     func goToConnexion( recognizer: UITapGestureRecognizer ) {
         if recognizer.state == .Ended {
-            performSegueWithIdentifier( "goToConnexion", sender: nil )
+            UIView.animateWithDuration( 0.5, delay: 0.0, options: .TransitionNone, animations: {
+                self.customTitle.alpha = 0.0
+                self.welcomeText.alpha = 0.0
+                self.subtitleText.alpha = 0.0
+                self.line.opacity = 0.0
+                self.tipDetails.alpha = 0.0
+            }, completion: { finished in
+                self.performSegueWithIdentifier( "goToConnexion", sender: nil )
+            } )
         }
     }
 }
